bf0009b80a8357af52f35a7fd3ffd1bb9125a092
Feven Yohannes <fyohannes@college.harvard.edu>
2020-11-01 20:47:26 -0500

    done?

--- FinalPredictions.R
+++ FinalPredictions.R
@@ -3,6 +3,7 @@
 # Packages
 
 library(readr)
+library(statebins)
 library(tidyr)
 library(ggplot2)
 library(dplyr)
@@ -381,65 +382,241 @@ pred1 <- pred %>%
 
 pred1
 
+pred1 %>%
+  mutate(avg_dem=mean(fit))
+
 
 # MAP
 
+states_map <- map_data("state")
+unique(states_map$region)
 
-map <- ggplot(predictions$dem_ec, aes(long, lat, group = group)) +
-  geom_polygon(aes(fill = dem_ec), color = "black") +
+
+#  states_map$state<- state.abb[match(states_map$state, state.name)]
+
+# Changing Pred dataframe to full state names to merge data
+
+pred1$state <-state.name[match(pred1$state, state.abb)]
+
+pred1$region <- tolower(pred1$state)
+  
+
+prediction_map <- pred1 %>%
+  left_join(states_map, by = "region")
+
+# Creating win margins 
+
+map_prediction_dataframe <- prediction_map %>%
+  mutate(R_pv = (100 - fit)) %>%
+  mutate(win_margin = (fit -R_pv)) %>%
+  mutate(dem_win = ifelse(win_margin >= -2, TRUE, FALSE))
+
+
+map <- map_prediction_dataframe %>% ggplot(aes(long, lat, group = group)) +
+  geom_polygon(aes(fill = win_margin), color = "black") +
   scale_fill_gradient2(
-    high = "red2", 
-    mid = "white",
+    high = "steelblue4", 
     name = "win margin",
-    low = "dodgerblue2", 
+    low = "red", 
     breaks = c(-50,-25,0,25,50), 
     limits=c(-52,52)
   ) +
   theme_void()  
+
+map
+
 
 # Adding state abbreivations 
 
 centroids <- data.frame(region=tolower(state.name), long=state.center$x, lat=state.center$y)
 centroids$abb<-state.abb[match(centroids$region,tolower(state.name))]
 
+# Extra Map stuff
+
+map2 <- map  +
+  
+  # Labeling the states so they fit the map and don't overlap
+  
+  geom_label(data = centroids,
+             aes(x = long,y = lat, label = abb),
+             force = 5, hjust = 1, size = 11,
+             inherit.aes = F
+  ) +
+  
+  
+  # Add titles and different additions
+  
+  labs(fill =" Democratic Win Margin") +
+  labs(title ="2020 Electoral College Predictions")+
+  ggtitle("2020 Electoral College Predictions") +
+  theme(plot.title = element_text(face = "bold",size=38)) +
+  theme(plot.caption =element_text(face = "bold",size=32)) +
+  theme(legend.title = element_text(color = "gray1", size = 29),legend.key.width = unit(1.9,"cm"),legend.key.size = unit(1.9, "cm")) + theme(legend.text=element_text(size=24)) 
+
+
+ggsave("Electoral_Map.png", height = 16, width = 21)
 
 
 
+# New Electoral Map with State Bins
+
+ggplot(map_prediction_dataframe, aes(state = state, fill = dem_win)) + 
+  geom_statebins(lbl_size = 10) + 
+  theme_statebins() +
+  scale_fill_manual(values=c("#F8766D","#619CFF")) +
+  labs(title = "2020 Presidential Electoral College Map",
+       subtitle = "Predicted by Poll data and Economic fundamentals",
+       fill = "") + 
+  theme(plot.title = element_text(size = 38, hjust = 0.5),
+        plot.subtitle = element_text(size = 32, hjust = 0.5))
+  guides(fill=FALSE)
+
+
+ggsave("Electoral_Map2.png", height = 16, width = 21)
 
 
 
+plot_usmap(data = map_prediction_dataframe, regions = "states", values = "win_margin") + 
+  theme_void() +
+  scale_fill_gradient2(
+    high = "blue",
+    mid = "white",
+    low = "red", 
+    breaks = c(-60, -40, -20, 0, 20, 40), 
+    limits = c(-61, 50),
+    name = "Win Margin") + 
+  labs(title = "Predicted Win Margin of Two-Party Popular Vote Share",
+       subtitle = "Weighted ensemble using historical polling accuracy 3-weeks out from 
+       election day (0.1) and white population proportion (0.9)",
+       caption = "Win margin is difference between Democratic and Republican 
+       two-party popular vote share in each state.") + 
+  theme(plot.title = element_text(size = 38, hjust = 0.5),
+        plot.subtitle = element_text(size = 32, hjust = 0.5))
+
+
+# Confidence Intervals for state by state prediction 
+
+CI_function_se <- function(i){
+  s_glm_econ2 <- econ2_glm(i)
+  s_glm_poll <- poll_glm(i)
+  s_glm_econ1 <- econ1_glm(i)
+
+  
+  Spoll <- new_data %>%
+    select(state,avg_support)
+  
+  
+  S_GDP <- new_data %>%
+    select(state,GDP_growth_qt)
+  
+  
+  S_fed <- new_data %>%
+    select(state,growth_rate)
 
 
 
+# loop through all states
 
+prediction_CI_se <- 0.92*predict(s_glm_poll, Spoll, interval = "prediction", level = 0.95, type = "link", se.fit = TRUE)$se.fit + 
+  0.02*predict(s_glm_econ1, S_fed, interval = "prediction", level = 0.95, type = "link", se.fit = TRUE)$se.fit + 
+  0.06*predict(s_glm_econ2, S_GDP, interval = "prediction", level = 0.95, type = "link", se.fit = TRUE)$se.fit  
+  
 
+}
 
+CI_function_fit <- function(i){
+  s_glm_econ2 <- econ2_glm(i)
+  s_glm_poll <- poll_glm(i)
+  s_glm_econ1 <- econ1_glm(i)
+  
+  
+  Spoll <- new_data %>%
+    select(state,avg_support)
+  
+  
+  S_GDP <- new_data %>%
+    select(state,GDP_growth_qt)
+  
+  
+  S_fed <- new_data %>%
+    select(state,growth_rate)
 
-
-
-
-
-
-
-
-
-
-# Map data
-
-states_map <- ggplot2::map_data("state")
-
-map_theme = function() {
-  theme(axis.title = element_blank()) +
-    theme(axis.text = element_blank()) +
-    theme(axis.ticks = element_blank()) + 
-    theme(legend.title = element_blank()) +
-    theme(panel.grid.major = element_blank())
+  
+  
+  # loop through all states
+  
+  prediction_CI_se <- 0.92*predict(s_glm_poll, Spoll, interval = "prediction", level = 0.95, type = "link", se.fit = TRUE)$se.fit + 
+    0.02*predict(s_glm_econ1, S_fed, interval = "prediction", level = 0.95, type = "link", se.fit = TRUE)$se.fit + 
+    0.06*predict(s_glm_econ2, S_GDP, interval = "prediction", level = 0.95, type = "link", se.fit = TRUE)$se.fit  
+  
   
 }
 
-states_list <- c("AL","AK","AZ","AR","CA","CO","CT","DE","FL","GA","HI","ID","IL","IN","IA","KS","KY","LA","ME","MD","MA","MI","MN","MS","MO","MT","NE","NV","NH","NJ","NM","NY","NC","ND","OH","OK","OR","PA","RI","SC","SD","TN","TX","UT","VT","VA","WA","WV","WI","WY")
-                 
-                 
+for (i in states_list){
+  CI_prediction_se <- CI_function_se(i)
+  CI_prediction_fit <- CI_function_fit(i)
+  
+  critval <- 1.96 ## approx 95% CI
+  
+  states_predictions$lwr[states_predictions$state == i] <- CI_prediction_fit - (critval * CI_prediction_se)
+  states_predictions$uppr[states_predictions$state == i] <- CI_prediction_fit + (critval * CI_prediction_se)
+}
+
+
+# Mutating my state predicitons
+
+states_predictions %>%
+  mutate(uppr=as.numeric(uppr)) %>%
+  mutate(lwr=as.numeric(lwr)) %>%
+  mutate(fit=as.numeric(fit))
+
+states_predictions %>%
+  group_by(state) %>%
+  mutate(lower= sum(fit,lwr),drop = FALSE) %>%
+  mutate(upper= sum(fit,uppr,drop = FALSE))
+
+
+# Graphing the uncertainity
+
+states_predictions$lwr <- unlist(states_predictions$lwr)
+
+states_predictions$uppr <- unlist(states_predictions$uppr)
+
+plotCI(x=1:num.reps,y=y,li=ci[1,],ui=ci[2,],col=z,lwd=3,ylim=c(2,4))
+
+p1 <- ggplot(states_predictions, aes(x=state,y=fit))+
+  geom_point()+
+  geom_line(data=states_predictions)+
+  geom_ribbon(data=states_predictions,aes(ymin=lwr,ymax=uppr),alpha=0.3)
+
+p1
+
+
+trial <- states_predictions %>%
+  select(state,fit,uppr,lwr)
+
+# GT Table
+
+
+gt(trial) %>%
+  tab_header(
+    title = "State Predicitions and Predicted Confidence Intervals",
+    subtitle = "2020 Presidential Election"
+  ) %>%
+  fmt_number(columns=2:4,decimals = 4) 
+
+trial
+
+
+
+
+
+
+
+
+
+
+
 
 
 
