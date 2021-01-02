8c76374340780da71a8d0e93b1850d4bfd58082d
Feven Yohannes <fyohannes@college.harvard.edu>
2021-01-02 13:14:59 -0500

    commit 1

---
+++ Narratives.R
@@ -0,0 +1,32 @@
+# Narratives
+
+# Packages
+
+library(readr)
+library(statebins)
+library(tidyr)
+library(ggplot2)
+library(dplyr)
+library(maps)
+library(googlesheets4)
+library(usmap)
+library(cowplot)
+library(grid)
+library(gridExtra)
+library(statebins)
+library(ggrepel)
+
+# For the labeling of my map graph
+
+library(broom)
+library(plotrix)
+
+# Need for standard error 
+
+library(statebins)
+library(googlesheets4)
+library(gt)
+
+# For the regression summary table 
+
+library(tidyverse)
